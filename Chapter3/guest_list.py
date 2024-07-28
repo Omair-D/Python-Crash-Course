90779268eae56b09a53b3839de0310906f36d69d
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-28 02:14:02 -0400

    Exercise 3-4

---
+++ Chapter3/guest_list.py
@@ -0,0 +1,9 @@
+guestList = ['kanye west', 'drake', 'lil wayne']
+
+# print invites using list
+
+invite = ", please join us for dinner!"
+
+print(f"{guestList[0].title()}{invite}")
+print(f"{guestList[1].title()}{invite}")
+print(f"{guestList[2].title()}{invite}")
