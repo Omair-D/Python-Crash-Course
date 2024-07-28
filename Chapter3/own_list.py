8302f51616eaa9992b214878cece2323e9ee85fd
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-28 00:54:21 -0400

    Exercise 3-3

--- Chapter3/own_list.py
+++ Chapter3/own_list.py
@@ -1,9 +1,9 @@
 cars = ['Lamborhini', 'BMW', 'Porsche', 'Ferrari']
 
-print(f"I would like to drive a", cars[0])
+print("I would like to drive a", cars[0])
 
-print(f"I would like to repair my", cars[1])
+print("I would like to repair my", cars[1])
 
-print(f"I would like to race a", cars[2])
+print("I would like to race a", cars[2])
 
-print(f"I would like to gift my mom a", cars[3])
+print("I would like to gift my mom a", cars[3])
