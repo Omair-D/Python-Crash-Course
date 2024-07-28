de5baa28b7b1fff94096c17253764f63e278b1a3
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-28 00:49:29 -0400

    Exercise 2-3

---
+++ Chapter3/greetings.py
@@ -0,0 +1,17 @@
+names = ['Adam', 'Ben', 'Caleb', 'Drake', 'Eric']
+
+messageAdam = ", I love your pet dog"
+messageBen = ", I hope you're doing well!"
+messageCaleb = ", I hope that your fish is doing well!"
+messageDrake = ", hey man keep it up with the music!"
+messageEric = ", it has been so long! Hope you are well!"
+
+print(f"{names[0]}{messageAdam}")
+
+print(f"{names[1]}{messageBen}")
+
+print(f"{names[2]}{messageCaleb}")
+
+print(f"{names[3]}{messageDrake}")
+
+print(f"{names[4]}{messageEric}")
