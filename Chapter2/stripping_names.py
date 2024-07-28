91ea60250433c1dc6d0a4b017ab43ea7001170c8
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-28 00:01:44 -0400

    Exercise 2-7

---
+++ Chapter2/stripping_names.py
@@ -0,0 +1,14 @@
+name = "\tomair\n   "
+print(name)
+
+# Left Strip
+print("-------")
+print(name.lstrip())
+
+# Right strip
+print("------")
+print(name.rstrip())
+
+# Strip all whitespace
+print("------")
+print(name.strip())
