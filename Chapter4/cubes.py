bb2b1cd0e74dd82aa91d3f119d9e8ee74edf5eea
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 02:46:30 -0400

    Exercise 4-8

---
+++ Chapter4/cubes.py
@@ -0,0 +1,12 @@
+# Make a list of the first 10 cubes (1-10)
+cubes = []
+
+for value in range(1, 11):
+	cubes.append(value**3)
+
+# Check if list is complete
+print(cubes)
+
+# use for loop to print out the value of each cube
+for value in cubes:
+	print(value)
