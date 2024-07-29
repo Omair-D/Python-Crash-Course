f827127a3cc907700a0a07dd37a19a45f79c3d2b
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 02:31:04 -0400

    Exercise 4-5

---
+++ Chapter4/summing_million.py
@@ -0,0 +1,9 @@
+# Make a list from one to one million
+million = list(range(1, 1000001))
+
+# Use min() and max() to see if it actually starts with one and ends in one million
+print(min(million))
+print(max(million))
+
+# Use sum function to see how fast python can add a million numbers
+print(sum(million))
