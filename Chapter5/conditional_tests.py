0b4f07b4b49bc486861c19c2d51ced4a7b34688f
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 05:32:49 -0400

    Exercise 5-1

---
+++ Chapter5/conditional_tests.py
@@ -0,0 +1,47 @@
+# Write series of conditional test along with own prediction (At least 5 false and 5 true)
+
+# False conditionals
+print("False conditionals:\n-----------------")
+
+car = 'porsche'
+print("Is the car a lamborghini? I predict false")
+print(car == 'lamborghini')
+
+language = 'english'
+print("Is the language Arabic? I predict false")
+print(language == 'arabic')
+
+phone = 'android'
+print("Is the phone an Iphone? I predict false")
+print(phone == 'iphone')
+
+guess = 17
+print("Is the guess 20? I predict false")
+print(guess == 20)
+
+hungry = False
+print("Is the man hungry? I predict false")
+print(hungry == True)
+
+# True conditionals
+print("True conditionals:\n-----------------")
+
+car = 'ferrari'
+print("Is the car a ferrari? I predict true")
+print(car == 'ferrari')
+
+language = 'korean'
+print("Is the language Korean? I predict true")
+print(language == 'korean')
+
+phone = 'iphone'
+print("Is the phone an Iphone? I predict true")
+print(phone == 'iphone')
+
+guess = 50
+print("Is the guess 50? I predict true")
+print(guess == 50)
+
+hungry = True
+print("Is the man hungry? I predict true")
+print(hungry == True)
