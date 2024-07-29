4716f007623282e7ee91c19c6f13f31c3080cb09
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 01:07:05 -0400

    Exercise 3-9

---
+++ Chapter3/dinner_guests.py
@@ -0,0 +1,7 @@
+guestList = ['kanye west', 'drake', 'lil wayne']
+
+# Use len() to print a message of the number of people invited to dinner
+
+numGuests = len(guestList)
+
+print('The number of people coming to dinner tonight is:',numGuests)
