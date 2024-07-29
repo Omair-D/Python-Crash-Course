f803e0d757650041ffa5023d795f33137d14d4bd
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 04:13:37 -0400

    Exercise 4-10

---
+++ Chapter4/slices.py
@@ -0,0 +1,10 @@
+guestList = ['kanye west', 'drake', 'lil wayne', 'ken carson', 'beyonce', 'brent faiyaz']
+
+# Print first three items from list
+print(f"The first three items in list are: {guestList[:3]}")
+
+# Print middle of the list items
+print(f"The middle of the list is: {guestList[2:4]}")
+
+# Print last three items of list
+print(f"The last three items in list are: {guestList[3:]}")
