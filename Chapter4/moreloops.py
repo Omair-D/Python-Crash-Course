b99821d0edcbfa7ab39a09889d1e7c232e77135b
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 04:24:46 -0400

    Exercise 4-12

---
+++ Chapter4/moreloops.py
@@ -0,0 +1,19 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+ # print("My favorite foods are:")
+ # print(my_foods)
+
+ # print("\nMy friend's favorite foods are:")
+ # print(friend_foods)
+
+ # Writing two for loops to print contents of each list
+print("My favorite foods are: ")
+
+for foods in my_foods:
+ 	print(foods)
+
+print("\nMy friend's favorite foods are: ")
+
+for foods in friend_foods:
+	print(foods)
