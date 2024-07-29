a032b14bbd12e9056558b9582eb7da456356fb9e
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 02:01:40 -0400

    Exercise 4-2

---
+++ Chapter4/animals.py
@@ -0,0 +1,14 @@
+# Store animals with common characteristics in a list
+animals = ['bunny', 'cat', 'hamster']
+
+# Loop printing names of each pet
+for pet in animals:
+	print(pet.title())
+
+# Modify loop to write a message about each pet
+for pet in animals:
+	print(f"{pet.title()} would make an amazing pet!")
+	
+# Add message outside of loop about these pets
+print("Any of these animals would be great pets.")
+
