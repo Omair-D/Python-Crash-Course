92fb0caa384c93e810e676886b257ab2db856489
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-29 01:57:05 -0400

    Exercise 4-1

--- Chapter4/pizzas.py
+++ Chapter4/pizzas.py
@@ -6,8 +6,8 @@ for pizza in pizzas:
 	print(pizza.title())
 
 # Modify loop to write a message along with the item
-
 for pizza in pizzas:
 	print(f"{pizza.title()} is so yummy!")
+	
 # Write message outside the for loop for my love of pizza
 print("I love all of these pizzas so much!")
