d82df3e1a473426daa6622d74cf18b3dd3e4c13d
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-30 02:53:34 -0400

    Exercise 6-2

---
+++ Chapter6/favNumbers.py
@@ -0,0 +1,21 @@
+# Store a dictionary of peoples favorite numbers
+favNumbers = {
+   'zack': 5,
+   'ashley': 10,
+   'cody': 17,
+   'ryan': 25,
+   'maria': 42
+}
+
+zackNumber = favNumbers['zack']
+ashleyNumber = favNumbers['ashley']
+codyNumber = favNumbers['cody']
+ryanNumber = favNumbers['ryan']
+mariaNumber = favNumbers['maria']
+
+# Print each person's name and their favorite number
+print(f"Zack's favorite number is: {zackNumber}")
+print(f"Ashley's favorite number is: {ashleyNumber}")
+print(f"Cody's favorite number is: {codyNumber}")
+print(f"Ryan's favorite number is: {ryanNumber}")
+print(f"Maria's favorite number is {mariaNumber}")
