0abd05b961444489d5f7260bd0465b5e82143b47
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-30 00:08:58 -0400

    Exercise 5-5

---
+++ Chapter5/alien_color3.py
@@ -0,0 +1,32 @@
+# Create variable alien_color and assign a color
+alien_color = 'green'
+
+# Write an if-elif-else chain printing for each color
+
+# 5 points elif chain
+if alien_color == 'green':
+	print('You have earned 5 points!')
+elif alien_color == 'yellow':
+	print('You have earned 10 points!')
+else:
+	print('You have earned 15 points')
+
+# 10 points elif chain
+alien_color = 'yellow'
+
+if alien_color == 'green':
+	print('You have earned 5 points!')
+elif alien_color == 'yellow':
+	print('You have earned 10 points!')
+else:
+	print('You have earned 15 points')
+
+# 15 points elif chain
+alien_color = 'red'
+
+if alien_color == 'green':
+	print('You have earned 5 points!')
+elif alien_color == 'yellow':
+	print('You have earned 10 points!')
+else:
+	print('You have earned 15 points')
