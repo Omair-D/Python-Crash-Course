8c8fde765a5ab11758096f1399627c41e57890ed
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-30 03:03:12 -0400

    Exercise 6-3

---
+++ Chapter6/glossary.py
@@ -0,0 +1,21 @@
+# Store 5 words and their meanings in a dictionary
+glossary = {
+	'variable': 'a named storage location in memory that holds data or objects',
+	'string': 'A series of characters.',
+	'list': 'A collection of items in a particular order.',
+	'comment': 'A note in a program that the Python interpreter ignores.',
+	'dictionary': "A collection of key-value pairs.",
+}
+
+defVar = glossary['variable']
+defString = glossary['string']
+defList = glossary['list']
+defComment = glossary['comment']
+defDictionary = glossary['dictionary']
+
+# Print each word and its meaning neatly
+print(f"Variable:\n\t{defVar}\n")
+print(f"String:\n\t{defString}\n")
+print(f"List:\n\t{defList}\n")
+print(f"Comment:\n\t{defComment}\n")
+print(f"Dictionary:\n\t{defDictionary}\n")
