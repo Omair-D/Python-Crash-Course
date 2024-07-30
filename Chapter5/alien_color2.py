b114a735dbd988607e0972da2f7543af3f45be07
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-30 00:04:28 -0400

    Exercise 5-4

---
+++ Chapter5/alien_color2.py
@@ -0,0 +1,17 @@
+# Create variable alien_color and assign a color
+alien_color = 'red'
+
+# if else chain
+if alien_color == 'green':
+	print('You have just earned 5 points!')
+else:
+	print('You have just earned 10 points!')
+
+# Write a version that runs the if block not the else block
+alien_color = 'green'
+
+# if else chain (The output will be 5 points)
+if alien_color == 'green':
+	print('You have just earned 5 points!')
+else:
+	print('You have just earned 10 points!')
