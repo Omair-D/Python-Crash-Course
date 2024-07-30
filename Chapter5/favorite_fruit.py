614c580b8d676a0186351e7a081a1ccf56093446
Omair <157149682+Omair-D@users.noreply.github.com>
2024-07-30 00:21:41 -0400

    Exercise 5-7

---
+++ Chapter5/favorite_fruit.py
@@ -0,0 +1,19 @@
+# Make a list of three favorite fruits
+favFruits = ['star fruit', 'apple', 'strawberry']
+
+# Check if fruit is in the list using five if statements
+
+if 'star fruit' in favFruits:
+	print("You really like star fruits!")
+
+if 'pineapple' in favFruits:
+	print("You really like pineapples!")
+
+if 'apple' in favFruits:
+	print("You really like apples!")
+
+if 'banana' in favFruits:
+	print("You really like bananas!")
+
+if 'strawberry' in favFruits:
+	print("You really like strawberries!")
