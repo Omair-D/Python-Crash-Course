b1f469002ac4dd0a0b9cf3d5b9f0e29a374017be
Omair <157149682+Omair-D@users.noreply.github.com>
2024-08-02 15:49:34 -0400

    Exercise 8-1

---
+++ Chapter8/message.py
@@ -0,0 +1,8 @@
+# Define function display_message()
+def display_message():
+    """Display simple sentence"""
+    # Print a sentence
+    print("I am learning how to use functions in Python!")
+
+# Call function and make sure it prints
+display_message()
