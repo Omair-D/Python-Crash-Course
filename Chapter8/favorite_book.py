da8728b6a2ab8dcec338a74841edbb0d251d7e83
Omair <157149682+Omair-D@users.noreply.github.com>
2024-08-02 15:49:57 -0400

    Exercise 8-2

---
+++ Chapter8/favorite_book.py
@@ -0,0 +1,5 @@
+# Define function favorite_book() To print message stating your favorite book
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+favorite_book('Harry Potter')
