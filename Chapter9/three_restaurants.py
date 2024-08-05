d0cf9e16b5b9441dbf1ff31b3f2b665bc2219b51
Omair <157149682+Omair-D@users.noreply.github.com>
2024-08-05 19:47:05 -0400

    Exercise 9-2

---
+++ Chapter9/three_restaurants.py
@@ -0,0 +1,34 @@
+class Restaurant:
+    """Simple attempt to replicate restaurant"""
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        """Method describing restuarant"""
+        print(f"\nThe restaurant is: {self.restaurant_name}")
+        print(f"The cuisine type is: {self.cuisine_type}")
+    
+    def open_restaurant(self):
+        """Method simulating restaurant open"""
+        print(f"\n{self.restaurant_name} is now open!")
+
+# Printing both attributes of class
+restaurant = Restaurant('Red Lobster', 'Seafood')
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+# Calling both methods in the class
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+# Make two more instances and and call describe_restaurant() for all of them
+restaurant2 = Restaurant('Chipotle', 'Mexican food')
+restaurant3 = Restaurant('A1 Japan', 'Japanese food')
+
+# Call new instances
+restaurant2.describe_restaurant()
+restaurant3.describe_restaurant()
+
+
+
