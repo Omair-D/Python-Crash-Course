7e9d28f9ebfcca8de4c870688f15a81575b04d94
Omair <157149682+Omair-D@users.noreply.github.com>
2024-08-13 06:49:47 -0400

    Exercise 11-1 & 11-2

---
+++ ~Chapter11/city_functions.py
@@ -0,0 +1,6 @@
+def city_country(city, country, population=5000000):
+    """Return the city and country name in neat format"""
+    output_string = f"{city.title()}, {country.title()}"
+    output_string += f" -population {population}"
+
+    return f"{city.title()}, {country.title()}"
